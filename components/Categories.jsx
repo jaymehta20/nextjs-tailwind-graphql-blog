34f9d48fcac296ed7fe6ca5746a061afb6cce65f
Jay Mehta <jake2001007@gmail.com>
2021-11-28 16:40:23 +0530

    implemented all the blog functionality except comments

---
+++ components/Categories.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+
+import { getCategories } from "../services";
+
+const Categories = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories().then((newCategories) => setCategories(newCategories));
+  }, []);
+
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-8 mb-8 pb-12">
+      <h3 className="text-xl mb-8 font-semibold border-b pb-2">Categories</h3>
+      {categories.map((category) => (
+        <div key={category.slug}>
+          <Link href={`/category/${category.slug}`}>
+            <span className="cursor-pointer block pb-3 mb-3">
+              {category.name}
+            </span>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Categories;
